45f9a73caa492261b42c3ef0b5b67bae2ac10925
Akash Singh <akashsingh6280@gmail.com>
2021-09-08 02:58:28 +0530

    ADDED: method to validate username using regex

---
+++ src/com/bridgelabz/emailvalidation/Email.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.emailvalidation;
+import java.util.regex.*;
+import java.util.Scanner;
+public class Email {
+	static Scanner scanner=new Scanner(System.in);
+	
+	public static void emailValidator() {
+		System.out.println("Enter email id");
+		String emailId=scanner.nextLine();
+		
+		boolean result = Pattern.matches("^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z0-9]", emailId);
+		if(result) {
+			System.out.println("Valid");
+		}
+		else {
+			System.out.println("Invalid");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		emailValidator();
+		
+	}
+}
